58387d58662829cc88c9d9a3cb5f42656fc553a6
jgmswt <st147811@stud.uni-stuttgart.de>
2018-01-25 07:58:33 +0100

    Refined logging

--- src/main/java/de/ustutt/iaas/cc/core/GoogleDatastoreNotebookDAO.java
+++ src/main/java/de/ustutt/iaas/cc/core/GoogleDatastoreNotebookDAO.java
@@ -56,6 +56,8 @@ public class GoogleDatastoreNotebookDAO implements INotebookDAO {
 			result.add(note);
 		}
     	
+		logger.info("Loaded " + result.size() + " notes from google datastore");
+		
     	return result;
     }
 
